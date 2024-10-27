cc4038d6cf870c189452c62052c386d02442fcac
33ronak <08arnab98@gmail.com>
2024-10-27 14:24:11 +0530

    useContext hook applied

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-// Write your code at relevant places in the code below:
-
 import React, { useEffect, useState } from 'react';
-
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     const storedUserLoggedInStatus = localStorage.getItem("isLoggedIn")
-       
+
     if (storedUserLoggedInStatus === "1") {
       setIsLoggedIn(true);
     }
   }, []);
-  
+
   const loginHandler = (email, password) => {
     setIsLoggedIn(true);
     localStorage.setItem("isLoggedIn", "1");
@@ -29,11 +28,16 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+      }}>
+        <MainHeader/>
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   );
 }
